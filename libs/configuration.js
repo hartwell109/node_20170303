e1517baf491fc2cb05ab8cc28166df7293a304ba
lin.peng <lin.peng@all-union.com.cn>
2017-03-08 10:57:05 +0800

    20170303:

---
+++ libs/configuration.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Mars on 2017/3/8.
+ * mongodb://<dbuser>:<dbpassword>@ds021989.mlab.com:21989/mongodb_test_20160324
+ */
+var configuration = {
+    dbUser: 'root',
+    dbPassword: 'admin',
+    dbUrl: 'ds021989.mlab.com',
+    dbPort: '21989',
+    dbName: 'mongodb_test_20160324'
+}
+module.exports = configuration
