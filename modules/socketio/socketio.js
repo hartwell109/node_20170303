c51d8d483d6aeea6638f521341d4afeb40f2b36b
lin.peng <lin.peng@all-union.com.cn>
2017-07-07 11:17:21 +0800

    20170707

--- modules/socketio/socketio.js
+++ modules/socketio/socketio.js
@@ -13,6 +13,13 @@ io.on('connection', function (socket) {
             payload: data,
             timestamp: new Date()
         }
+        /**
+         * 回复客户端消息
+         */
+        socket.emit('toClient', msg)
+        /*
+         将消息发送到主进程
+         */
         process.send(msg);
     })
 })
